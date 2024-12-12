f31c70ff84a0bb6ec85e567d59e3be45b2788582
Ayodele Temitope <dyenamics228@gmail.com>
2024-12-12 14:14:01 +0100

    change images to better ones

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,44 +1,10 @@
-// import { RiTwitterXFill, RiTelegram2Fill } from "react-icons/ri";
-
-// const Footer = () => {
-//   return (
-//     <footer className="linear">
-//       <div className="max-w-[1350px] mx-auto pt-52 pb-32 ">
-//         <h1 className="lg:text-[9rem] text-blue-600 mb-10 font-bold leading-[1] text-center">
-//           Ski Mask panda
-//         </h1>
-//         <h2 className="text-center font-bold text-[2rem] mb-20">
-//           $SKIPA is based
-//         </h2>
-//         <div className="flex justify-center gap-3">
-//           <a
-//             href=""
-//             className="py-3 px-2 flex items-center justify-center bg-black rounded-xl"
-//           >
-//             {" "}
-//             <RiTwitterXFill color="white" className="text-4xl" />
-//           </a>
-//           <a
-//             href=""
-//             className="py-3 px-2 flex items-center justify-center bg-black rounded-xl"
-//           >
-//             {" "}
-//             <RiTelegram2Fill color="white" className="text-4xl" />
-//           </a>
-//         </div>
-//       </div>
-//     </footer>
-//   );
-// };
-
-// export default Footer;
-
+import skipandas from "../assets/skipandas.webp";
 import { RiTwitterXFill, RiTelegram2Fill } from "react-icons/ri";
 
 const Footer = () => {
   return (
     <footer className="linear">
-      <div className="max-w-[1350px] mx-auto pt-32 pb-10 px-4">
+      <div className="max-w-[1350px] mx-auto pt-32 px-4">
         <h1 className="font-edo text-[3rem] sm:text-[5rem] lg:text-[9rem] text-blue-600 mb-10 font-bold leading-[1] text-center">
           Ski Mask Panda
         </h1>
@@ -60,12 +26,7 @@ const Footer = () => {
           </a>
         </div>
 
-        <a
-          href="https://www.skipabase.fun/"
-          className="font-edo text-center text-white text-[1.5rem] mt-16 flex justify-center"
-        >
-          skipabase.fun
-        </a>
+        <img src={skipandas} alt="" />
       </div>
     </footer>
   );
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,87 +1,5 @@
-// // import proof from "../assets/proof.webp";
-// import panda from "../assets/panda.PNG";
-// import { useState } from "react";
-// import { FaRegCopy } from "react-icons/fa6";
-// import { FaCopy } from "react-icons/fa6";
-// import dex from "../assets/dex-screener.png";
-
-// const Hero = () => {
-//   const [hasCopied, setHasCopied] = useState(false);
-
-//   const handleCopy = () => {
-//     navigator.clipboard.writeText("0xc66B839Ef20f2D35B3B7b0f6d908Ab63a9829674");
-//     setHasCopied(true);
-
-//     setTimeout(() => {
-//       setHasCopied(false);
-//     }, 2000);
-//   };
-//   return (
-//     <div className="max-w-[1350px] mx-auto pt-20 mb-20">
-//       <div className="grid grid-cols-2 gap-2">
-//         <div>
-//           <h1 className="lg:text-[9rem] text-blue-600 mb-10 font-bold leading-[1]">
-//             Ski Mask panda
-//           </h1>
-//           <h2 className="text-black mb-8 text-[2rem] font-extrabold ">
-//             SKIPA - MASK STAYS ON
-//           </h2>
-//           <h3 className="uppercase mb-8 text-[1.5rem] text-blue-600 font-bold ">
-//             Contract address
-//           </h3>
-//           <div
-//             onClick={handleCopy}
-//             className="flex justify-center gap-4 items-center bg-blue-600 border border-b-[7px] border-b-black rounded-2xl py-3 px-4 w-fit"
-//           >
-//             {hasCopied ? (
-//               <h2 className="text-white font-bold text-2xl">Copied!</h2>
-//             ) : (
-//               <h2 className="text-2xl text-white font-bold">
-//                 0xc66B839Ef20f2D35B3B7b0f6d908Ab63a9829674
-//               </h2>
-//             )}
-
-//             <div>
-//               {hasCopied ? (
-//                 <FaCopy className="text-3xl " />
-//               ) : (
-//                 <FaRegCopy className="text-3xl" />
-//               )}
-//             </div>
-//           </div>
-
-//           <div className="flex mt-8 gap-1">
-//             <div className="bg-blue-600 py-3 px-3 flex border border-b-[7px] border-b-black rounded-2xl gap-2">
-//               <img
-//                 src={dex}
-//                 alt=""
-//                 width={40}
-//                 height={40}
-//                 className="rounded-full"
-//               />
-//               <p className="text-white text-[2rem] font-bold">DEX Screener</p>
-//             </div>
-//             <a
-//               href=""
-//               className="flex justify-center text-white font-bold items-center text-[2rem] bg-blue-600 border border-b-[7px] border-b-black rounded-2xl p-3"
-//             >
-//               Buy $SKIPA
-//             </a>
-//           </div>
-//         </div>
-
-//         <div>
-//           <img src={panda} alt="" />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Hero;
-
-// import proof from "../assets/proof.webp";
-import panda from "../assets/panda.webp";
+// import panda from "../assets/panda.webp";
+import skipanda from "../assets/skipanda.webp";
 import { useState } from "react";
 import { FaRegCopy } from "react-icons/fa6";
 import { FaCopy } from "react-icons/fa6";
@@ -115,7 +33,7 @@ const Hero = () => {
 
           <div
             onClick={handleCopy}
-            className="flex justify-center gap-2 items-center bg-blue-600 border border-b-[7px] border-b-black rounded-2xl py-3 px-4 w-full sm:w-fit"
+            className="flex justify-center gap-2 items-center bg-blue-600 border border-b-[7px] border-b-black rounded-2xl py-3 px-4 w-full sm:w-fit cursor-pointer"
           >
             {hasCopied ? (
               <h2 className="text-white font-bold text-xl sm:text-2xl">
@@ -129,15 +47,18 @@ const Hero = () => {
 
             <div>
               {hasCopied ? (
-                <FaCopy className="text-2xl sm:text-3xl" />
+                <FaCopy className="text-2xl sm:text-3xl text-white" />
               ) : (
-                <FaRegCopy className="text-2xl sm:text-3xl" />
+                <FaRegCopy className="text-2xl sm:text-3xl text-white" />
               )}
             </div>
           </div>
 
           <div className="flex flex-wrap mt-8 gap-1 justify-center lg:justify-start">
-            <div className="bg-blue-600 py-3 px-4 flex border border-b-[7px] border-b-black rounded-2xl gap-2 items-center">
+            <a
+              href=""
+              className="bg-blue-600 py-3 px-4 flex border border-b-[7px] border-b-black rounded-2xl gap-2 items-center"
+            >
               <img
                 src={dex}
                 alt="DEX Screener"
@@ -148,7 +69,7 @@ const Hero = () => {
               <p className="text-white text-lg sm:text-[2rem] font-bold">
                 DEX Screener
               </p>
-            </div>
+            </a>
             <a
               href=""
               className="flex justify-center items-center text-white font-bold text-lg sm:text-[2rem] bg-blue-600 border border-b-[7px] border-b-black rounded-2xl py-3 px-4"
@@ -160,7 +81,7 @@ const Hero = () => {
 
         <div className="flex justify-center">
           <img
-            src={panda}
+            src={skipanda}
             alt="Panda"
             className="w-full max-w-md lg:max-w-full animate-float"
           />
